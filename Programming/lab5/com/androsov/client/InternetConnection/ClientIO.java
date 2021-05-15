4058c808d6e67a6ab572473a577f5e2d0b5c6bc8
HaCK3R322 <70976294+HaCK3R322@users.noreply.github.com>
2021-05-15 16:39:51 +0300

    hello

--- Programming/lab5/com/androsov/client/InternetConnection/ClientIO.java
+++
@@ -1,9 +0,0 @@
-package com.androsov.client.InternetConnection;
-
-import java.io.IOException;
-
-public interface ClientIO {
-    void connectToServer(String ipAddress, int port) throws IOException;
-    void sendCommandLine(String line) throws IOException;
-    String getResponse() throws IOException;
-}
